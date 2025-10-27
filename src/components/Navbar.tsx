8dcce63e58b0222899498008be4e386b743a7ef6
[dyad] <git@dyad.sh>
2025-10-27 12:45:26 +0530

    [dyad] Added smooth animations to dropdown menus - wrote 1 file(s)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -107,7 +107,10 @@ const Navbar = () => {
                             <ChevronDown className="ml-1 h-4 w-4" />
                           </Button>
                         </DropdownMenuTrigger>
-                        <DropdownMenuContent className="w-80 p-4 bg-muted" align="start">
+                        <DropdownMenuContent
+                          className="w-80 p-4 bg-muted data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+                          align="start"
+                        >
                           {item.heading && (
                             <DropdownMenuLabel className="px-3 py-2 text-xs font-semibold uppercase text-muted-foreground">
                               {item.heading}
