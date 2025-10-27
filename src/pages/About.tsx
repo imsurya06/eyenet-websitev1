12fd10292c078a25b1b4c2ebe89b00283b8d2958
[dyad] <git@dyad.sh>
2025-10-27 16:16:56 +0530

    [dyad] Added the WhyChooseUsSection to the About page - wrote 1 file(s)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import AboutHeroSection from '@/components/AboutHeroSection';
+import WhyChooseUsSection from '@/components/WhyChooseUsSection'; // Import the WhyChooseUsSection
 
 const About = () => {
   return (
     <>
       <AboutHeroSection />
+      <WhyChooseUsSection /> {/* Add the WhyChooseUsSection here */}
     </>
   );
 };
