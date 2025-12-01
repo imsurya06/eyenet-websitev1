58ae424d28220dbed89e2ed1133336f45bfd9fb5
[dyad] <surya2930m@gmail.com>
2025-12-01 23:03:30 +0530

    [dyad] Fixed 'React.Children.only' error in AdminInfrastructure filter - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/admin/AdminInfrastructure.tsx
+++ src/pages/admin/AdminInfrastructure.tsx
@@ -73,7 +73,9 @@ const AdminInfrastructure = () => {
               className="px-4 py-2 text-text-regular border-primary text-primary hover:bg-primary hover:text-primary-foreground"
               // Removed onMouseEnter and onMouseLeave
             >
-              Filter <Filter className="ml-2 h-4 w-4" />
+              <span> {/* Wrap children in a span */}
+                Filter <Filter className="ml-2 h-4 w-4" />
+              </span>
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent
